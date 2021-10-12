5946c53d31c8cfcc993c500e05ee4afce807df88
Alvin Zhao <alvin.zhao@myob.com>
2021-10-12 17:24:22 +1100

    add home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Form from "./Form";
+import Home from "./Home";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Home />
+      <Form />
+    </>
+  );
 }
 
 export default App;
---
+++ src/Home.tsx
@@ -0,0 +1,14 @@
+const Home = () => {
+  return (
+    <>
+      <img src="./logo192.png" alt="logo" />
+      <h1>Hello namehere</h1>
+      <p>
+        All the following questions relate to the week of 2021-10-01 to
+        2021-10-06
+      </p>
+    </>
+  );
+};
+
+export default Home;
